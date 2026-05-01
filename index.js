6a8d549940ae0d4e8ee8ed5ef0f1cfbef1a5d847
45leoos-hue <45leoos@gmail.com>
2026-05-01 22:17:47 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,17 @@
 // 🔧 SETUP
 // ======================
 require('dotenv').config();
-const { Client, GatewayIntentBits, EmbedBuilder } = require('discord.js');
+const { 
+    Client, 
+    GatewayIntentBits, 
+    EmbedBuilder,
+    ActionRowBuilder,
+    StringSelectMenuBuilder,
+    ButtonBuilder,
+    ButtonStyle,
+    ChannelType,
+    PermissionsBitField
+} = require('discord.js');
 
 const client = new Client({
     intents: [
@@ -19,6 +29,15 @@ process.on('uncaughtException', console.error);
 // ======================
 // 📌 KONSTANTEN
 // ======================
+const teamAllowedRoleId = '1492883325415850084';
+const banAllowedRoleId = '1499776740669653073';
+const banLogChannelId = '1499778326422753380';
+const welcomeChannelId = '1499839976316211272';
+
+
+
+
+
 const teamRoles = [
     '1492679028132548679',
     '1492681437755867136',
@@ -66,10 +85,45 @@ async function generateTeamEmbed(guild) {
 
     return new EmbedBuilder()
         .setColor('#023fb9')
-        .setTitle('👥 Leoo Teamliste')
+        .setTitle('<:emoji1:1499846963070373888> Leoo Teamliste')
         .setImage('https://cdn.discordapp.com/attachments/1494369413724508260/1498722832526475405/ChatGPT_Image_28._Apr._2026_18_05_00.png')
         .setDescription(description || '❌ Keine Teams gefunden');
 }
+
+
+
+// ======================
+// 🎉 WILLKOMMEN EVENT (NEU)
+// ======================
+client.on('guildMemberAdd', async member => {
+    try {
+        const channel = member.guild.channels.cache.get(welcomeChannelId);
+        if (!channel) return;
+
+        const embed = new EmbedBuilder()
+            .setTitle('Sutana | Leoo Settings')
+            .setDescription(
+                `<:herz1:1499852268273537054> **Willkommen ${member} auf ${member.guild.name}**\n\n` +
+
+                `| <:emoji1:1499846963070373888> **Memberanzahl:** ${member.guild.memberCount}\n` +
+                `| <:regeln1:1499853243704938536> **Regeln:** <#1492714385666347109>\n` +
+                `| <:haken1:1499859150681935982> **Verify:** <#1492722075612483684>\n\n\n` +
+                
+                
+                '**Danke, dass du beigetreten bist!**' 
+            
+            
+            )
+            .setColor('#0020ff')
+            .setImage('https://cdn.discordapp.com/attachments/1499848010950115430/1499863185249931495/Leoo_Welcome.png?ex=69f657f1&is=69f50671&hm=6fe82a57a17403af55a424322692a1d1916c3fd28024550b00087ec6b8090d08&')
+            .setThumbnail(member.user.displayAvatarURL());
+
+        await channel.send({ embeds: [embed] });
+
+    } catch (err) {
+        console.error("WELCOME ERROR:", err);
+    }
+});
 
 
 // ======================
@@ -114,63 +168,113 @@ client.once('clientReady', async () => {
 // ======================
 client.on('interactionCreate', async interaction => {
     if (!interaction.isChatInputCommand()) return;
-
-    // 🔥 STOP falls schon verarbeitet
     if (interaction.replied || interaction.deferred) return;
 
     try {
         await interaction.deferReply({ flags: 64 });
 
         // ======================
-        // 👥 TEAM JOIN
+        // 👥 TEAM JOIN COMMAND
         // ======================
         if (interaction.commandName === 'teamjoin') {
 
+            const member = await interaction.guild.members.fetch(interaction.user.id);
+
+            if (!member.roles.cache.has(teamAllowedRoleId)) {
+                return interaction.editReply({
+                    content: "❌ Du hast keine Berechtigung für diesen Command!"
+                });
+            }
+
             const user = interaction.options.getUser('user');
             const role = interaction.options.getRole('role');
+            const reason = interaction.options.getString('grund');
+            const grussrolle = interaction.options.getRole('grußrolle');
 
-                
-            const grußrolle = interaction.options.getRole('grußrolle');
+            if (!user || !role) {
+                return interaction.editReply({
+                    content: '❌ User oder Rolle fehlt!'
+                });
+            }
 
-            const target = await interaction.guild.members.fetch(user.id);
-
-            await target.roles.add(role);
+            let target;
+            try {
+                target = await interaction.guild.members.fetch(user.id);
+                await target.roles.add(role);
+            } catch (err) {
+                console.error("ROLE ERROR:", err);
+            }
 
             const embed = new EmbedBuilder()
-                .setTitle("🔵 TEAMUPDATE 🔵")
+                .setTitle("✅ Team Beitritt !")
                 .setDescription(
                     `Hiermit tritt <@${user.id}> dem Team als <@&${role.id}> bei!\n\n` +
-                    `**Grund:** ${interaction.options.getString('grund') || 'Kein Grund angegeben'}\n\n` +
+                    `**Grund:** ${reason || 'Kein Grund angegeben'}\n\n` +
                     `**Wir wünschen dir viel Spaß im Team**\n\n` +
                     `Mit freundlichen Grüßen` +
-                    (grußrolle ? `\n<@&${grußrolle.id}>` : '')
+                    (grussrolle && grussrolle.id ? `\n<@&${grussrolle.id}>` : '')
                 )
                 .setColor("#0020ff");
 
             await interaction.channel.send({ embeds: [embed] });
 
             return interaction.editReply({ content: '✅ Erfolgreich gesendet' });
-        }
+            }
 
+        // ======================
+        // 🚪 TEAM LEAVE COMMAND
+        // ======================
         else if (interaction.commandName === 'teamleave') {
 
-            const user = interaction.options.getUser('user');
-            const role = interaction.options.getRole('team');
-            const target = await interaction.guild.members.fetch(user.id);
+            const member = await interaction.guild.members.fetch(interaction.user.id);
 
-            if (!target.roles.cache.has(role.id)) {
+            if (!member.roles.cache.has(teamAllowedRoleId)) {
                 return interaction.editReply({
-                    content: "❌ Dieser User ist nicht im Team."
+                    content: "❌ Du hast keine Berechtigung für diesen Command!"
                 });
             }
 
-            await target.roles.remove(role);
+            const user = interaction.options.getUser('user');
+            const role = interaction.options.getRole('ehemalige_rolle');
+            const reason = interaction.options.getString('grund');
+            const grussrolle = interaction.options.getRole('grußrolle');
+
+            const teamRoleId = "1493696482015187024";
+
+            if (!user || !role) {
+                return interaction.editReply({
+                    content: '❌ User oder Rolle fehlt!'
+                });
+            }
+
+            let target;
+            try {
+                target = await interaction.guild.members.fetch(user.id);
+
+                if (!target.roles.cache.has(role.id)) {
+                    return interaction.editReply({
+                        content: "❌ Dieser User ist nicht in dieser Rolle!"
+                    });
+                }
+
+                await target.roles.remove(role);
+
+                if (target.roles.cache.has(teamRoleId)) {
+                    await target.roles.remove(teamRoleId);
+                }
+
+            } catch (err) {
+                console.error("ROLE ERROR:", err);
+            }
 
             const embed = new EmbedBuilder()
-                .setTitle("🔵 TEAMUPDATE 🔵")
+                .setTitle("❌ Team Austritt !")
                 .setDescription(
-                    `Hiermit verlässt <@${user.id}> das Team <@&${role.id}>.\n\n` +
-                    `Mit freundlichen Grüßen\n<@&1492883079763595407>`
+                    `Hiermit verlässt <@${user.id}> das Team als <@&${role.id}>.\n\n` +
+                    `**Grund:** ${reason || 'Kein Grund angegeben'}\n\n` +
+                    `**Wir wünschen dir alles Gute**\n\n` +
+                    `Mit freundlichen Grüßen` +
+                    (grussrolle && grussrolle.id ? `\n<@&${grussrolle.id}>` : '')
                 )
                 .setColor("#0020ff");
 
@@ -179,51 +283,127 @@ client.on('interactionCreate', async interaction => {
             return interaction.editReply({ content: '✅ Erfolgreich gesendet' });
         }
 
+        // ======================
+        // ⬆️ UPRANK COMMAND
+        // ======================
         else if (interaction.commandName === 'uprank') {
 
-            const user = interaction.options.getUser('user');
-            const role = interaction.options.getRole('role');
-            const target = await interaction.guild.members.fetch(user.id);
-            const botMember = interaction.guild.members.me;
+            const member = await interaction.guild.members.fetch(interaction.user.id);
 
-            if (role.position >= botMember.roles.highest.position) {
+            if (!member.roles.cache.has(teamAllowedRoleId)) {
                 return interaction.editReply({
-                    content: "❌ Rolle ist über meiner Bot-Rolle."
+                    content: "❌ Du hast keine Berechtigung für diesen Command!"
                 });
             }
-
-            if (!target.manageable) {
-                return interaction.editReply({
-                    content: "❌ Ich darf diesen User nicht bearbeiten."
-                });
-            }
-
-            await target.roles.add(role);
-
-            return interaction.editReply({
-                content: `⬆️ ${user.tag} wurde hochgestuft (${role.name})`
-            });
-        }
-
-        else if (interaction.commandName === 'downrank') {
 
             const user = interaction.options.getUser('user');
-            const role = interaction.options.getRole('role');
-            const target = await interaction.guild.members.fetch(user.id);
+            const oldRole = interaction.options.getRole('alte_rolle');
+            const newRole = interaction.options.getRole('neue_rolle');
+            const reason = interaction.options.getString('grund');
+            const grussrolle = interaction.options.getRole('grußrolle');
 
-            if (!target.manageable) {
+            if (!user || !oldRole || !newRole) {
                 return interaction.editReply({
-                    content: "❌ Ich darf diesen User nicht bearbeiten."
+                    content: "❌ User oder Rollen fehlen!"
                 });
             }
 
-            await target.roles.remove(role);
+            const target = await interaction.guild.members.fetch(user.id);
 
-            return interaction.editReply({
-                content: `⬇️ ${user.tag} wurde heruntergestuft (${role.name})`
-            });
+            if (!target.roles.cache.has(oldRole.id)) {
+                return interaction.editReply({
+                    content: "❌ User hat die alte Rolle nicht!"
+                });
+            }
+
+            await target.roles.remove(oldRole);
+            await target.roles.add(newRole);
+
+            const embed = new EmbedBuilder()
+                .setTitle("⬆️ UPRANK !")
+                .setDescription(
+                    `Hiermit bekommt <@${user.id}> seinen Uprank.\n\n Von <@&${oldRole.id}> \n Auf <@&${newRole.id}> \n\n` +
+                    `**Grund:** ${reason || 'Kein Grund angegeben'}\n\n` +
+                    `Mit freundlichen Grüßen` +
+                    (grussrolle && grussrolle.id ? `\n<@&${grussrolle.id}>` : '')
+                )
+                .setColor("#0020ff");
+
+            await interaction.channel.send({ embeds: [embed] });
+
+            return interaction.editReply({ content: '✅ Erfolgreich gesendet' });
         }
 
+        // ======================
+        // ⬇️ DERANK COMMAND
+        // ======================
+        else if (interaction.commandName === 'derank') {
+
+            const member = await interaction.guild.members.fetch(interaction.user.id);
+
+            if (!member.roles.cache.has(teamAllowedRoleId)) {
+                return interaction.editReply({
+                    content: "❌ Du hast keine Berechtigung für diesen Command!"
+                });
+            }
+
+            try {
+
+                const user = interaction.options.getUser('user');
+                const oldRole = interaction.options.getRole('alte_rolle');
+                const newRole = interaction.options.getRole('neue_rolle');
+                const reason = interaction.options.getString('grund');
+                const grussrolle = interaction.options.getRole('grußrolle');
+
+                if (!user || !oldRole || !newRole) {
+                    return interaction.editReply({
+                        content: "❌ User oder Rollen fehlen!"
+                    });
+                }
+
+                const target = await interaction.guild.members.fetch(user.id);
+
+                if (!target.roles.cache.has(oldRole.id)) {
+                    return interaction.editReply({
+                        content: "❌ User hat die ausgewählte alte Rolle nicht!"
+                    });
+                }
+
+                if (target.roles.cache.has(newRole.id)) {
+                    return interaction.editReply({
+                        content: "❌ User hat die neue Rolle bereits!"
+                    });
+                }
+
+                await target.roles.remove(oldRole);
+                await target.roles.add(newRole);
+
+                const embed = new EmbedBuilder()
+                    .setTitle("⬇️ DERANK !")
+                    .setDescription(
+                        `Hiermit bekommt <@${user.id}> seinen Derank. \n\n Von <@&${oldRole.id}> \n Auf <@&${newRole.id}> \n\n` +
+                        `**Grund:** ${reason || 'Kein Grund angegeben'}\n\n` +
+                        `Mit freundlichen Grüßen` +
+                        (grussrolle ? `\n<@&${grussrolle.id}>` : '')
+                    )
+                    .setColor("#0020ff");
+
+                await interaction.channel.send({ embeds: [embed] });
+
+                return interaction.editReply({ content: '✅ Erfolgreich gesendet' });
+
+            } catch (err) {
+                console.error("DOWNRANK ERROR:", err);
+
+                return interaction.editReply({
+                    content: "❌ Fehler beim Derank!"
+                }).catch(() => {});
+            }
+        }
+
+        // ======================
+        // 📋 TEAMLISTE COMMAND
+        // ======================
         else if (interaction.commandName === 'teamliste') {
 
             const embed = await generateTeamEmbed(interaction.guild);
@@ -233,19 +413,123 @@ client.on('interactionCreate', async interaction => {
             return interaction.editReply({ content: '✅ Gesendet' });
         }
 
-    } catch (err) {
-        console.error("GLOBAL ERROR:", err);
+        // ======================
+        // 🔨 BAN
+        // ======================
+        else if (interaction.commandName === 'ban') {
 
-        if (interaction.deferred || interaction.replied) {
-            interaction.editReply({
-                content: "❌ Fehler aufgetreten"
-            }).catch(() => {});
+            const member = await interaction.guild.members.fetch(interaction.user.id);
+
+            if (!member.roles.cache.has(banAllowedRoleId)) {
+                return interaction.editReply({ content: "❌ Keine Berechtigung!" });
+            }
+
+            const user = interaction.options.getUser('user');
+            const reason = interaction.options.getString('grund');
+            const moderator = interaction.user;
+
+            if (!user) {
+                return interaction.editReply({ content: "❌ User fehlt!" });
+            }
+
+            let target = await interaction.guild.members.fetch(user.id);
+
+            try {
+                await user.send({
+                    embeds: [new EmbedBuilder()
+                        .setTitle("🚫 Du wurdest gebannt")
+                        .setDescription(`Du wurdest vom Server ${interaction.guild.name} gebannt!\n\n` +
+                                `Grund: ${reason || 'Kein Grund'}\n\n` +
+                                `Gebannt von: <@${moderator.id}>`)
+                        .setColor("#ff0000")]
+                });
+            } catch {}
+
+            await target.ban({ reason: reason || 'Kein Grund' });
+
+            const embed = new EmbedBuilder()
+                .setTitle("🔨 User gebannt")
+                .setDescription(`<@${user.id}> wurde vom Server ${interaction.guild.name} gebannt!\n\n` +
+                                `Grund: ${reason || 'Kein Grund'}\n\n` +
+                                `Gebannt von: <@${moderator.id}>`)
+                .setColor("#ff0000");
+
+            await interaction.channel.send({ embeds: [embed] });
+
+            const logChannel = await interaction.guild.channels.fetch(banLogChannelId).catch(() => null);
+            if (logChannel) await logChannel.send({ embeds: [embed] });
+
+            return interaction.editReply({ content: "✅ Gebannt" });
+        }
+
+        // ======================
+        // 🔓 UNBAN
+        // ======================
+        else if (interaction.commandName === 'unban') {
+
+            const member = await interaction.guild.members.fetch(interaction.user.id);
+
+            if (!member.roles.cache.has(banAllowedRoleId)) {
+                return interaction.editReply({ content: "❌ Keine Berechtigung!" });
+            }
+
+            const userId = interaction.options.getString('userid');
+            const reason = interaction.options.getString('grund');
+            const moderator = interaction.user;
+
+            if (!userId) {
+                return interaction.editReply({ content: "❌ User ID fehlt!" });
+            }
+
+            await interaction.guild.members.unban(userId);
+
+            try {
+                const user = await client.users.fetch(userId);
+                await user.send({
+                    embeds: [new EmbedBuilder()
+                        .setTitle("✅ Du wurdest entbannt")
+                        .setDescription(`Du wurdest vom Server ${interaction.guild.name} entbannt!\n\n` +
+                                `Grund: ${reason || 'Kein Grund'}\n\n` +
+                                `Entbannt von: <@${moderator.id}>`)
+                        .setColor("#3cff00")]
+                });
+            } catch (err) {
+              console.log("DM FEHLER:", err.message);
+      }
+
+            const embed = new EmbedBuilder()
+                .setTitle("🔓 User entbannt")
+                .setDescription(`<@${userId}> wurde vom Server ${interaction.guild.name} entbannt!\n\n` +
+                                `Grund: ${reason || 'Kein Grund'}\n\n` +
+                                `Entbannt von: <@${moderator.id}>`)
+                .setColor("#00ff00");
+
+            await interaction.channel.send({ embeds: [embed] });
+
+            const logChannel = await interaction.guild.channels.fetch(banLogChannelId).catch(() => null);
+            if (logChannel) await logChannel.send({ embeds: [embed] });
+
+            return interaction.editReply({ content: "✅ Entbannt" });
+        
+         }
+
+    } catch (err) {
+        console.error("COMMAND ERROR:", err);
+
+        if (!interaction.replied && !interaction.deferred) {
+            await interaction.reply({ content: "❌ Fehler", ephemeral: true }).catch(() => {});
+        } else {
+            await interaction.editReply({ content: "❌ Fehler" }).catch(() => {});
         }
     }
-});
+});    
+    
+        
+
+
 
 
 // ======================
 // 🔐 LOGIN
 // ======================
-client.login(process.env.DISCORD_BOT_TOKEN);
+client.login(process.env.DISCORD_BOT_TOKEN); 
