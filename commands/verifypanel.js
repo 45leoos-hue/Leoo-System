c35656d3f38cef931e37dbf96458c58284e25254
45leoos-hue <45leoos@gmail.com>
2026-05-10 23:28:01 +0200

    Update verifypanel.js

--- commands/verifypanel.js
+++ commands/verifypanel.js
@@ -1,6 +1,10 @@
-const { EmbedBuilder, ButtonBuilder, ButtonStyle, ActionRowBuilder } = require('discord.js');
+const { EmbedBuilder, ButtonBuilder, ButtonStyle, ActionRowBuilder, SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('verifypanel')
+        .setDescription('Sendet das Verify Panel'),
+
     name: 'verifypanel',
 
     async execute(interaction, client, config) {
@@ -9,15 +13,28 @@ module.exports = {
 
         const button = new ButtonBuilder()
             .setCustomId('verify_btn')
-            .setLabel('🔐 Verifizieren')
+            .setLabel('Klicke um zu Verifizieren')
             .setStyle(ButtonStyle.Success);
 
         const row = new ActionRowBuilder().addComponents(button);
 
         const embed = new EmbedBuilder()
-            .setTitle('✅ ・ Verify')
-            .setDescription('Klicke unten zum verifizieren')
-            .setColor('#0020ff');
+            .setTitle('**<:haken1:1501921978028785685>  Verify System**')
+            .setDescription(
+                
+
+                '** Willkommen auf 〨 | LEOO Settings** \n\n' +
+                
+                
+                '<:schloss1:1501921620711964783> **Warum verifizieren?** \n' +
+                '⚬ Zum Schutz vor Bots\n' +
+                '⚬ Damit du alle Channels sehen kannst\n\n' +
+                
+                '<:doppelpfeilrechts:1501921595743014963> **Klicke unten zum verifizieren**'
+            )    
+            .setColor('#0020ff')
+            .setImage('https://i.imgur.com/9AJYzUF_d.png?maxwidth=520&shape=thumb&fidelity=high')
+            .setThumbnail('https://i.imgur.com/oov4GnX_d.png?maxwidth=520&shape=thumb&fidelity=high');
 
         const channel = interaction.guild.channels.cache.get(config.verifyChannelId);
 
